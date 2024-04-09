6c572624e0499b7671840a8659ac62eb9570a292
Topo Fabbri <matufabbri@gmail.com>
2024-04-08 22:04:31 -0300

    Implemented NetConsole

---
+++ Assets/Scripts/UI/ChatScreen.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using UnityEngine.UI;
+
+public class ChatScreen : MonoBehaviourSingleton<ChatScreen>
+{
+    public Text messages;
+    public InputField inputMessage;
+
+    protected override void Initialize()
+    {
+        inputMessage.onEndEdit.AddListener(OnEndEdit);
+
+        this.gameObject.SetActive(false);
+
+        NetworkManager.Instance.OnReceiveEvent += OnReceiveDataEvent;
+    }
+
+    void OnReceiveDataEvent(byte[] data, IPEndPoint ep)
+    {
+        if (NetworkManager.Instance.isServer)
+        {
+            NetworkManager.Instance.Broadcast(data);
+        }
+
+        NetConsole message = new();
+        
+        messages.text += message.Deserialize(data);
+    }
+
+    void OnEndEdit(string str)
+    {
+        if (inputMessage.text != "")
+        {
+            NetConsole message = new();
+            message.data = str;
+            
+            if (NetworkManager.Instance.isServer)
+            {
+                NetworkManager.Instance.Broadcast(message.Serialize());
+                messages.text += inputMessage.text + System.Environment.NewLine;
+            }
+            else
+            {
+                NetworkManager.Instance.SendToServer(message.Serialize());
+            }
+
+            inputMessage.ActivateInputField();
+            inputMessage.Select();
+            inputMessage.text = "";
+        }
+
+    }
+
+}
