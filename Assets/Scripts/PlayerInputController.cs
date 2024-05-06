05e6a9e145d27e4b7a485f30a2f86ef0f4e38d6b
Topo Fabbri <matufabbri@gmail.com>
2024-05-06 20:05:46 -0300

    Added player with movement

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,20 +5,31 @@ public class PlayerInputController : MonoBehaviour
 {
     [SerializeField] private GameObject chatScreen;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private Transform camera;
     [SerializeField] private float accel;
     [SerializeField] private float maxSpeed;
-
+    [SerializeField] private float camSens;
+    [SerializeField] private float maxCamSpeed;
+    
     private Vector2 moveInput;
+    private float mouseX;
     
     private void OnToggleChat()
     {
         chatScreen.SetActive(!chatScreen.activeSelf);
+        Cursor.lockState = chatScreen.activeSelf ? CursorLockMode.None : CursorLockMode.Locked;
     }
 
     private void FixedUpdate()
     {
-        rb.AddForce(new Vector3(moveInput.x, 0, moveInput.y) * (accel * Time.deltaTime));
+        rb.AddForce((transform.forward * moveInput.y + transform.right * moveInput.x) * (accel * Time.fixedDeltaTime));
         rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+    }
+
+    private void Update()
+    {
+        Quaternion targetRotation = transform.rotation * Quaternion.Euler(0, mouseX * Time.deltaTime * camSens, 0);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, maxCamSpeed * Time.deltaTime);
     }
 
     private void OnMove(InputValue input)
@@ -27,5 +37,12 @@ public class PlayerInputController : MonoBehaviour
         if (chatScreen.activeSelf) return;
         
         moveInput = input.Get<Vector2>();
+    }
+
+    private void OnLook(InputValue input)
+    {
+        if (chatScreen.activeSelf) return;
+
+        mouseX = input.Get<Vector2>().x;
     }
 }
